55797941211218b3b089ab9ba9b7b2a3321b7cd0
walidmash <waleedmash2@gmail.com>
2019-06-06 02:42:09 +0200

    ff

--- views/js/script.js
+++ views/js/script.js
@@ -35,26 +35,43 @@ $('.datepicker.sample').datepicker({
 		taxCodeForm.addEventListener('submit',function(e){
 			e.preventDefault();
 			if($("#form").valid()) {
-				var fromInput = document.querySelector('#form_from');
-				alert(fromInput.value);
-				$("#form :input").prop("disabled", true);
-				taxCodeForm.classList.add('disabled');
+				$.ajax({
+					url: `/api/check-user-dates?user_id=${$("#taxCode").val()}&service_id=10&&from=${$("#form_from").val()}&to=${$("#form_to").val()}`,
+					success: function(result){
+						if(result.tax_code || result.user){ // TODO result.durationIsValid check
+							$('#username_span').html(result.user.name)
+							$("#form :input").prop("disabled", true);
+							taxCodeForm.classList.add('disabled');
+							$("#payment-form").removeClass('hidden');
+					}else{
+							alert('duratuion or user id is not available');
+						}
+					}});
+
+
 			}else{
 				alert("please input valid values");
 			}
 		})
 	}
 });
+function formBack(){
+	$("#form :input").prop("disabled", false);
+	document.querySelector("#form").classList.remove('disabled');
+	$("#payment-form").addClass('hidden');
+}
 function loadMyReservations(userId='tax_code2') {
 	resTable = document.querySelector('.myReservation-table tbody');
 	resTable.innerHTML = 'Loading...';
-	$.ajax({url: "http://localhost:4000/api/my-reservations?userId="+userId, success: function(result){
+	$.ajax({url: "/api/my-reservations?userId="+userId, success: function(result){
 		
 		resTable.innerHTML = result.reduce(function(acc,res){
+			var d = JSON.parse(res.duration);
+
 			return acc + `<tr>
 			<th scope="col">${moment(res.created_at).format('DD-MM-YYYY')}</th>
 			<th scope="col">${res.amount}</th>
-			<th scope="col">${JSON.parse(res.duration)}</th>
+			<th scope="col">${d?moment(d[0]).format('DD-MM-YYYY')+' to ':''}${d?moment(d[1]).format('DD-MM-YYYY'):''}</th>
 			<th scope="col">${res.type}</th>
 			<th scope="col">${res.state}</th>
 		  </tr>`
@@ -64,7 +81,7 @@ function loadMyReservations(userId='tax_code2') {
 function loadRooms() {
 	resTable = document.querySelector('#roomsList');
 	resTable.innerHTML = 'Loading...';
-	$.ajax({url: "http://localhost:4000/api/availabe-rooms", success: function(result){
+	$.ajax({url: "/api/availabe-rooms", success: function(result){
 		
 		resTable.innerHTML = result.reduce(function(acc,res){
 			return acc + `<div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
